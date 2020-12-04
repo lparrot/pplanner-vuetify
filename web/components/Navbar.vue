bc6afbfabf97422f04c396fa814df21197bbd700
lauparr <laurent.parrot78@gmail.com>
2020-12-04 22:54:37 +0100

    Modification de la barre de navigation

--- web/components/Navbar.vue
+++ web/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full bg-white">
+  <div v-on-clickaway="hide" class="w-full bg-white">
     <div class="flex flex-col px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
       <div class="p-4 flex flex-row items-center justify-between">
         <slot name="before-title"></slot>
@@ -9,24 +9,24 @@
           </slot>
         </nuxt-link>
         <slot name="after-title"></slot>
-        <button class="md:hidden rounded-lg focus:outline-none focus:shadow-outline" @click="open = !open">
+        <button class="md:hidden rounded-lg focus:outline-none focus:shadow-outline" @click="toggle">
           <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-            <path v-if="!open" clip-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" fill-rule="evenodd"></path>
+            <path v-if="!opened" clip-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" fill-rule="evenodd"></path>
             <path v-else clip-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" fill-rule="evenodd"></path>
           </svg>
         </button>
       </div>
-      <nav :class="[open ? 'flex': 'hidden', right && 'md:justify-end']" class="flex-col flex-grow md:pb-0 md:flex md:flex-row block bg-white z-50 relative top-0 left-0">
+      <nav :class="{'md:justify-end': right, 'flex': opened, 'hidden': !opened}" class="flex flex-col flex-grow md:pb-0 md:flex md:flex-row block bg-white z-50 relative top-0 left-0">
         <slot name="before-menu"></slot>
         <template v-if="show">
           <template v-for="(menuItem, menuItemIndex) in menu">
-            <NavbarItem v-if="menuItem.children == null" :key="menuItemIndex" :to="menuItem.path" exact>{{ menuItem.label }}</NavbarItem>
-            <Dropdown v-else :key="menuItemIndex" :items="menuItem.children" :label="menuItem.label" navbar></Dropdown>
+            <NavbarItem v-if="menuItem.children == null" :key="menuItemIndex" :to="menuItem.path" exact @click.native="hide">{{ menuItem.label }}</NavbarItem>
+            <Dropdown v-else :key="menuItemIndex" :items="menuItem.children" :label="menuItem.label" navbar @on-item-click="hide"></Dropdown>
           </template>
         </template>
         <slot name="after-menu"></slot>
       </nav>
-      <nav :class="[open ? 'flex': 'hidden']" class="flex flex-col block bg-white z-50 relative top-0 left-0 md:justify-end md:flex md:flex-row">
+      <nav :class="{'flex': opened, 'hidden': !opened}" class="flex flex-col block bg-white z-50 relative top-0 left-0 md:justify-end md:flex md:flex-row">
         <slot name="right"></slot>
       </nav>
     </div>
@@ -36,6 +36,7 @@
 <script>
 import Dropdown from '@/components/Dropdown'
 import NavbarItem from '@/components/NavbarItem'
+import { mixin as clickaway } from 'vue-clickaway'
 
 export default {
   name: 'Navbar',
@@ -47,6 +48,8 @@ export default {
       navbar: this,
     }
   },
+
+  mixins: [clickaway],
 
   props: {
     brandHref: {
@@ -81,8 +84,22 @@ export default {
 
   data () {
     return {
-      open: false,
+      opened: false,
     }
+  },
+
+  methods: {
+    hide () {
+      this.opened = false
+    },
+
+    open () {
+      this.opened = true
+    },
+
+    toggle () {
+      this.opened = !this.opened
+    },
   },
 }
 </script>
